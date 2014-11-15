511e6e37a107f027a72405128620ca449dfa33ac
George Burgess IV <george.burgess.iv@gmail.com>
2014-11-14 23:02:06 -0500

    gofmt'ed, +syn message support, +tests and docs.
    > Clients now respond to Syn connection requests
    > Added tests for client functionality
    > Ran gofmt across the codebase

--- client.go
+++ client.go
@@ -1,312 +1,316 @@
 package mp
 
 import (
-  "errors"
-  "strconv"
-  "io"
-  "sync"
-  "sync/atomic"
+	"errors"
+	"io"
+	"strconv"
+	"sync"
+	"sync/atomic"
 )
 
 const (
-  max64BitNumberCharsHex = 16 // 8 bytes * 2 chars per byte
+	max64BitNumberCharsHex = 16 // 8 bytes * 2 chars per byte
 )
 
 type clientConnection struct {
-  closed chan struct{}
-  isClosed bool
+	closed   chan struct{}
+	isClosed bool
 
-  messages chan []byte
-  currentMessage []byte
-  readLock sync.Mutex
+	messages       chan []byte
+	currentMessage []byte
+	readLock       sync.Mutex
 
-  seqNum uint64
-  otherClient string
-  connId string
-  client *Client
-  waitForConfirm bool
+	seqNum         uint64
+	otherClient    string
+	connId         string
+	client         *Client
+	waitForConfirm bool
 }
 
 var (
-  errWouldBlock = errors.New("Would block")
+	errWouldBlock = errors.New("Would block")
 )
 
 func newClientConnection(otherClient, connId string, client *Client) *clientConnection {
-  return &clientConnection{
-    // messages needs to remain unbuffered because of how our closed channel works.
-    messages: make(chan []byte),
-    closed: make(chan struct{}),
-    otherClient: otherClient,
-    connId: connId,
-    client: client,
-  }
+	return &clientConnection{
+		// messages needs to remain unbuffered because of how our closed channel works.
+		messages:    make(chan []byte),
+		closed:      make(chan struct{}),
+		otherClient: otherClient,
+		connId:      connId,
+		client:      client,
+	}
 }
 
 func (c *clientConnection) readIntoBuffer(blocking bool) error {
-  if c.currentMessage != nil {
-    return nil
-  }
+	if c.currentMessage != nil {
+		return nil
+	}
 
-  var msg []byte
-  if !blocking {
-    select {
-    case msg = <-c.messages:
-      break
-    case <-c.closed:
-      return io.EOF
-    default:
-      return errWouldBlock
-    }
-  } else {
-    select {
-    case msg = <-c.messages:
-      break
-    case <-c.closed:
-      return io.EOF
-    }
-  }
+	var msg []byte
+	if !blocking {
+		select {
+		case msg = <-c.messages:
+			break
+		case <-c.closed:
+			return io.EOF
+		default:
+			return errWouldBlock
+		}
+	} else {
+		select {
+		case msg = <-c.messages:
+			break
+		case <-c.closed:
+			return io.EOF
+		}
+	}
 
-  c.currentMessage = msg
-  return nil
+	c.currentMessage = msg
+	return nil
 }
 
 func (c *clientConnection) putNewMessage(msg *Message) bool {
-  select {
-  case <-c.closed:
-    return false
-  case c.messages <- msg.Data:
-    return true
-  }
+	select {
+	case <-c.closed:
+		return false
+	case c.messages <- msg.Data:
+		return true
+	}
 }
 
 func (c *clientConnection) Read(b []byte) (int, error) {
-  c.readLock.Lock()
-  defer c.readLock.Unlock()
+	c.readLock.Lock()
+	defer c.readLock.Unlock()
 
-  n := 0
-  remSlice := b
-  for n == 0 && len(remSlice) > 0 {
-    block := n == 0
-    err := c.readIntoBuffer(block)
-    if err == errWouldBlock {
-      return n, nil
-    } else if err != nil {
-      return n, err
-    }
+	n := 0
+	remSlice := b
+	for n == 0 && len(remSlice) > 0 {
+		block := n == 0
+		err := c.readIntoBuffer(block)
+		if err == errWouldBlock {
+			return n, nil
+		} else if err != nil {
+			return n, err
+		}
 
-    x := copy(remSlice, c.currentMessage)
-    if len(c.currentMessage) == x {
-      c.currentMessage = nil
-    } else {
-      c.currentMessage = c.currentMessage[x:]
-    }
-    remSlice = remSlice[x:]
-    n += x
-  }
+		x := copy(remSlice, c.currentMessage)
+		if len(c.currentMessage) == x {
+			c.currentMessage = nil
+		} else {
+			c.currentMessage = c.currentMessage[x:]
+		}
+		remSlice = remSlice[x:]
+		n += x
+	}
 
-  return n, nil
+	return n, nil
 }
 
 func (c *clientConnection) Write(b []byte) (int, error) {
-  if err := c.WriteMessage(b); err != nil {
-    return 0, err
-  }
-  return len(b), nil
+	if err := c.WriteMessage(b); err != nil {
+		return 0, err
+	}
+	return len(b), nil
 }
 
 func (c *clientConnection) Close() error {
-  c.isClosed = true
-  close(c.closed)
-  return nil
+	c.isClosed = true
+	close(c.closed)
+	return nil
 }
 
 func (c *clientConnection) ReadMessage() ([]byte, error) {
-  c.readLock.Lock()
-  defer c.readLock.Unlock()
+	c.readLock.Lock()
+	defer c.readLock.Unlock()
 
-  err := c.readIntoBuffer(true)
-  if err != nil {
-    return nil, err
-  }
+	err := c.readIntoBuffer(true)
+	if err != nil {
+		return nil, err
+	}
 
-  msg := c.currentMessage
-  c.currentMessage = nil
-  return msg, nil
+	msg := c.currentMessage
+	c.currentMessage = nil
+	return msg, nil
 }
 
 func (c *clientConnection) nextSeqNum() uint64 {
-  return atomic.AddUint64(&c.seqNum, 1)
+	return atomic.AddUint64(&c.seqNum, 1)
 }
 
 func (c *clientConnection) WriteMessage(b []byte) error {
-  msg := Message{
-    Meta: MetaNone,
-    SeqNum: c.nextSeqNum(),
-    OtherClient: c.otherClient,
-    Data: b,
-  }
+	msg := Message{
+		Meta:        MetaNone,
+		SeqNum:      c.nextSeqNum(),
+		OtherClient: c.otherClient,
+		Data:        b,
+	}
 
-  err := c.client.sendMessage(&msg)
-  if err != nil {
-    return err
-  }
+	err := c.client.sendMessage(&msg)
+	if err != nil {
+		return err
+	}
 
-  if c.waitForConfirm {
-    panic("TODO")
-  }
+	if c.waitForConfirm {
+		// This is a somewhat heavy item. Will deal with it later.
+		panic("TODO")
+	}
 
-  return nil
+	return nil
 }
 
 func (c *clientConnection) OtherClient() string {
-  return c.otherClient
+	return c.otherClient
 }
 
 func (c *clientConnection) SetWaitForConfirm(w bool) {
-  c.waitForConfirm = true
+	c.waitForConfirm = true
 }
 
 func (c *clientConnection) WaitsForConfirm() bool {
-  return c.waitForConfirm
+	return c.waitForConfirm
 }
 
 type Client struct {
-  name string
-  server io.ReadWriteCloser
-  serverRLock sync.Mutex
-  serverWLock sync.Mutex
+	name        string
+	server      io.ReadWriteCloser
+	serverRLock sync.Mutex
+	serverWLock sync.Mutex
 
-  translator MessageTranslator
+	newConnHandler NewConnectionHandler
+	translator     MessageTranslator
 
-  connections map[string]*clientConnection
-  connectionsLock sync.Mutex
+	connections     map[string]*clientConnection
+	connectionsLock sync.Mutex
 
-  waitingConnections map[string]*clientConnection
-  waitingConnectionsLock sync.Mutex
+	waitingConnections     map[string]*clientConnection
+	waitingConnectionsLock sync.Mutex
 
-  connNumber int64
-  authed bool
+	connNumber int64
+	authed     bool
 }
 
 func NewClient(
-  name string,
-  server io.ReadWriteCloser,
-  translator MessageTranslator) *Client {
+	name string,
+	server io.ReadWriteCloser,
+	translator MessageTranslator,
+	connHandler NewConnectionHandler) *Client {
 
-  return &Client{
-    name: name,
-    server: server,
-    serverRLock: sync.Mutex{},
-    serverWLock: sync.Mutex{},
+	return &Client{
+		name:        name,
+		server:      server,
+		serverRLock: sync.Mutex{},
+		serverWLock: sync.Mutex{},
 
-    translator: translator,
+		newConnHandler: connHandler,
+		translator:     translator,
 
-    connections: make(map[string]*clientConnection),
-    connectionsLock: sync.Mutex{},
+		connections:     make(map[string]*clientConnection),
+		connectionsLock: sync.Mutex{},
 
-    waitingConnections: make(map[string]*clientConnection),
-    waitingConnectionsLock: sync.Mutex{},
-  }
+		waitingConnections:     make(map[string]*clientConnection),
+		waitingConnectionsLock: sync.Mutex{},
+	}
 }
 
 func (c *Client) sendMessage(m *Message) error {
-  c.serverWLock.Lock()
-  defer c.serverWLock.Unlock()
+	c.serverWLock.Lock()
+	defer c.serverWLock.Unlock()
 
-  return c.translator.WriteTo(c.server, m)
+	return c.translator.WriteTo(c.server, m)
 }
 
 func (c *Client) recvMessage() (*Message, error) {
-  c.serverRLock.Lock()
-  defer c.serverRLock.Unlock()
+	c.serverRLock.Lock()
+	defer c.serverRLock.Unlock()
 
-  return c.translator.ReadFrom(c.server)
+	return c.translator.ReadFrom(c.server)
 }
 
 func (c *Client) Authenticate(password []byte) error {
-  if c.authed {
-    return errors.New("Multiple authentications requested")
-  }
+	if c.authed {
+		return errors.New("Multiple authentications requested")
+	}
 
-  msg := Message{
-    Meta: MetaAuth,
-    OtherClient: c.name,
-    Data: password,
-  }
+	msg := Message{
+		Meta:        MetaAuth,
+		OtherClient: c.name,
+		Data:        password,
+	}
 
-  err := c.sendMessage(&msg)
-  if err != nil {
-    return err
-  }
+	err := c.sendMessage(&msg)
+	if err != nil {
+		return err
+	}
 
-  resp, err := c.recvMessage()
-  if err != nil {
-    return err
-  }
+	resp, err := c.recvMessage()
+	if err != nil {
+		return err
+	}
 
-  if resp.Meta != MetaAuthOk {
-    return errors.New(string(resp.Data))
-  }
+	if resp.Meta != MetaAuthOk {
+		return errors.New(string(resp.Data))
+	}
 
-  return nil
+	return nil
 }
 
 func (c *Client) addConnection(conn *clientConnection) {
-  c.connectionsLock.Lock()
-  c.connections[conn.connId] = conn
-  c.connectionsLock.Unlock()
+	c.connectionsLock.Lock()
+	c.connections[conn.connId] = conn
+	c.connectionsLock.Unlock()
 }
 
 func (c *Client) nextConnId() string {
-  // In most cases there's no point in making garbage here.
-  nextInt := atomic.AddInt64(&c.connNumber, 1)
-  var microoptimization [64]byte
-  buf := microoptimization[:]
-  buf = append(buf, c.name...)
-  buf = append(buf, ':')
-  buf = strconv.AppendInt(buf, nextInt, 16)
-  return string(buf)
+	// In most cases there's no point in making garbage here.
+	nextInt := atomic.AddInt64(&c.connNumber, 1)
+	var microoptimization [64]byte
+	buf := microoptimization[:]
+	buf = append(buf, c.name...)
+	buf = append(buf, ':')
+	buf = strconv.AppendInt(buf, nextInt, 16)
+	return string(buf)
 }
 
 func (c *Client) MakeConnection(otherClient, proto string) (Connection, error) {
-  // Can I just use the machinery for sending a message using a Connection,
-  // then wait until I either get an error or response?
-  // Yes pls.
-  id := c.nextConnId()
-  conn := newClientConnection(otherClient, id, c)
+	// Can I just use the machinery for sending a message using a Connection,
+	// then wait until I either get an error or response?
+	// Yes pls.
+	id := c.nextConnId()
+	conn := newClientConnection(otherClient, id, c)
 
-  c.waitingConnectionsLock.Lock()
-  c.waitingConnections[id] = conn
-  c.waitingConnectionsLock.Unlock()
+	c.waitingConnectionsLock.Lock()
+	c.waitingConnections[id] = conn
+	c.waitingConnectionsLock.Unlock()
 
-  msg := &Message{
-    Meta: MetaConnSyn,
-    OtherClient: otherClient,
-    ConnectionId: id,
-    Data: []byte(proto),
-  }
+	msg := &Message{
+		Meta:         MetaConnSyn,
+		OtherClient:  otherClient,
+		ConnectionId: id,
+		Data:         []byte(proto),
+	}
 
-  err := c.sendMessage(msg)
-  if err != nil {
-    return nil, err
-  }
+	err := c.sendMessage(msg)
+	if err != nil {
+		return nil, err
+	}
 
-  // When we get our initial message, that means our connection has been moved from 
-  // c.waitingConnections to c.connections for us. Hooray.
-  data, err := conn.ReadMessage()
-  if err != nil {
-    return nil, err
-  } else if data != nil {
-    return nil, errors.New("Expected nil data ReadMessage()")
-  }
-  return conn, nil
+	// When we get our initial message, that means our connection has been moved from
+	// c.waitingConnections to c.connections for us. Hooray.
+	data, err := conn.ReadMessage()
+	if err != nil {
+		return nil, err
+	} else if data != nil {
+		return nil, errors.New("Expected nil data ReadMessage()")
+	}
+	return conn, nil
 }
 
 func (c *Client) findConnection(id string) (*clientConnection, bool) {
-  c.connectionsLock.Lock()
-  a, b := c.connections[id]
-  c.connectionsLock.Unlock()
-  return a, b
+	c.connectionsLock.Lock()
+	a, b := c.connections[id]
+	c.connectionsLock.Unlock()
+	return a, b
 }
 
 // return (bool, error) is admittedly a bit weird. We'll update
@@ -317,74 +321,93 @@ func (c *Client) findConnection(id string) (*clientConnection, bool) {
 // Note that this function has free reign to update msg however it sees fit.
 // Make a deep copy if you don't want it updated.
 func (c *Client) handleMetaMessage(msg *Message) (resp bool, err error) {
-  msg.Data = nil
-  switch msg.Meta {
-    case MetaNone:
-      panic("Passed unmeta message to handleMetaMessage")
-    case MetaAuth, MetaAuthOk, MetaAuthFailure:
-      msg.Meta = MetaWAT
-      return true, nil
-    case MetaConnSyn:
-      panic("TODO")
-    case MetaConnAck:
-      cid := msg.ConnectionId
-      c.waitingConnectionsLock.Lock()
-      conn, ok := c.waitingConnections[cid]
-      if ok {
-        delete(c.waitingConnections, cid)
-      }
-      c.waitingConnectionsLock.Unlock()
+	// In most cases, it's okay to bounce data back with our message, but it's
+	// also undesirable to do so. So we nil out msg.Data and keep a snapshot of
+	// what the data is, in case we explicitly want to send back the data that was
+	// sent (or somehow use the data)
+	msgData := msg.Data
+	msg.Data = nil
+	switch msg.Meta {
+	case MetaNone:
+		panic("Passed unmeta message to handleMetaMessage")
+	case MetaAuth, MetaAuthOk, MetaAuthFailure:
+		msg.Meta = MetaWAT
+		return true, nil
+	case MetaConnSyn:
+		id := c.nextConnId()
+		conn := newClientConnection(msg.OtherClient, id, c)
+		proto := string(msgData)
+		ok := c.newConnHandler.IncomingConnection(proto, conn)
+		if !ok {
+			msg.Meta = MetaUnknownProto
+			return true, nil
+		}
 
-      if !ok {
-        msg.Meta = MetaNoSuchConnection
-        return true, nil
-      }
+		c.connectionsLock.Lock()
+		c.connections[id] = conn
+		c.connectionsLock.Unlock()
 
-      c.connectionsLock.Lock()
-      c.connections[cid] = conn
-      c.connectionsLock.Unlock()
+		msg.Meta = MetaConnAck
+		return true, nil
+	case MetaConnAck:
+		cid := msg.ConnectionId
+		c.waitingConnectionsLock.Lock()
+		conn, ok := c.waitingConnections[cid]
+		if ok {
+			delete(c.waitingConnections, cid)
+		}
+		c.waitingConnectionsLock.Unlock()
 
-      conn.putNewMessage(msg)
-      return false, nil
-    default:
-      return false, errors.New("Unknown meta message type passed into handleMetaMessage")
-  }
+		if !ok {
+			msg.Meta = MetaNoSuchConnection
+			return true, nil
+		}
+
+		c.connectionsLock.Lock()
+		c.connections[cid] = conn
+		c.connectionsLock.Unlock()
+
+		conn.putNewMessage(msg)
+		return false, nil
+	default:
+		return false, errors.New("Unknown meta message type passed into handleMetaMessage")
+	}
 }
 
 func (c *Client) Run() error {
-  if !c.authed {
-    return errors.New("Need to authenticate before running the client")
-  }
+	if !c.authed {
+		return errors.New("Need to authenticate before running the client")
+	}
 
-  defer c.server.Close()
-  for {
-    msg, err := c.recvMessage()
-    if err != nil {
-      return err
-    }
+	defer c.server.Close()
+	for {
+		msg, err := c.recvMessage()
+		if err != nil {
+			return err
+		}
 
-    if msg.Meta == MetaNone {
-      conn, ok := c.findConnection(msg.ConnectionId)
-      if !ok || !conn.putNewMessage(msg) {
-        msg.Data = nil
-        msg.Meta = MetaNoSuchConnection
-        err = c.sendMessage(msg)
-        if err != nil {
-          return err
-        }
-      }
-    } else {
-      respond, err := c.handleMetaMessage(msg)
-      if err != nil {
-        return err
-      }
+		if msg.Meta == MetaNone {
+			conn, ok := c.findConnection(msg.ConnectionId)
+			if !ok || !conn.putNewMessage(msg) {
+				msg.Data = nil
+				msg.Meta = MetaNoSuchConnection
+				err = c.sendMessage(msg)
+				if err != nil {
+					return err
+				}
+			}
+		} else {
+			respond, err := c.handleMetaMessage(msg)
+			if err != nil {
+				return err
+			}
 
-      if respond {
-        err = c.sendMessage(msg)
-        if err != nil {
-          return err
-        }
-      }
-    }
-  }
+			if respond {
+				err = c.sendMessage(msg)
+				if err != nil {
+					return err
+				}
+			}
+		}
+	}
 }
--- connection.go
+++ connection.go
@@ -1,43 +1,95 @@
 package mp
 
 import (
-  "io"
+	"io"
 )
 
 type MetaType uint8
 
 const (
-  MetaNone = MetaType(iota)
-  MetaWAT // Invalid/unknown meta type received
-  MetaNoSuchConnection
-  MetaConnSyn
-  MetaConnAck
-  MetaAuth
-  MetaAuthOk
-  MetaAuthFailure
+	MetaNone = MetaType(iota)
+	MetaWAT  // Invalid/unknown meta type received
+	MetaNoSuchConnection
+	MetaConnSyn
+	MetaConnAck
+	MetaAuth
+	MetaAuthOk
+	MetaAuthFailure
+	MetaUnknownProto
 )
 
+type MessageFlags uint8
+
+const (
+	FlagNone            = MessageFlags(0)
+	FlagNeedReadReceipt = MessageFlags(1 << iota)
+)
+
+// The raw Message type. This and the above Meta types are fully exposed in
+// order to make rolling your own MessageTranslator/etc. as effortless as
+// possible. The Client/Server types generally shouldn't expose raw Message
+// instances directly to the user.
 type Message struct {
-  Meta MetaType
-  SeqNum uint64
-  OtherClient string
-  ConnectionId string
-  Data []byte
+	Meta         MetaType
+	Flags        MessageFlags
+	SeqNum       uint64
+	OtherClient  string
+	ConnectionId string
+	Data         []byte
 }
 
+// A duplexed message-passing connection. Connections can operate in two modes:
+// WaitForConfirm, and not WaitForConfirm.
+//
+// In Not-WaitForConfirm mode, writes will return as soon as the underlying
+// Write() implementation pushes the Write out.
+//
+// In WaitForConfirm mode, the Connection will block in Write() until we receive
+// a notification that the Connection you're writing the message to has Read
+// the message.
+//
+// Some notes for clarity:
+//  > If writes in WaitForConfirm mode recieve errors, it may be that the other
+//    Connection got the message, but never got the chance to respond.
+//    WaitForConfirm *only* guarantees that the write was received. Not that it
+//    wasn't.
+//  > In either mode, messages are guaranteed to be sent in the order you
+//    Write them. So, as long as the underlying Reader/Writer implementations
+//    deliver messages in order, messages will be delivered in order.
+//  > The mode you're in should have no bearing on how quickly you receive a
+//    notification that the other side has called Close()
 type Connection interface {
-  io.ReadWriteCloser
+	// Note that Read([]byte) *is allowed* to concatenate messages
+	// and, as a result, may drop messages with empty data. If you
+	// expect to be able to receive messages with empty data, please
+	// use ReadMessage.
+	//
+	// Similarly, Write() is allowed to merge requests into larger
+	// Messages in order to increase efficiency and such.
+	io.ReadWriteCloser
 
-  ReadMessage() ([]byte, error)
-  WriteMessage([]byte) error
+	ReadMessage() ([]byte, error)
+	WriteMessage([]byte) error
 
-  OtherClient() string
+	OtherClient() string
 
-  SetWaitForConfirm(bool)
-  WaitsForConfirm() bool
+	SetWaitForConfirm(bool)
+	WaitsForConfirm() bool
 }
 
+// TODO: This name sucks.
+// Also, a callback would probably be "easier" in this scenario, however
+// I expect that any use with more than a few supported services would need to
+// keep a fair amount of state with those services, so it's likely better for
+// the user to just implement an interface.
+type NewConnectionHandler interface {
+	IncomingConnection(string, Connection) bool
+}
+
+// A type that can read a message from a Reader and write a message to a Writer
+// in some format. Instances of MessageTranslator are not expected to be
+// used concurrently.
 type MessageTranslator interface {
-  ReadFrom(io.Reader) (*Message, error)
-  WriteTo(io.Writer, *Message) error
+	ReadFrom(io.Reader) (*Message, error)
+	WriteTo(io.Writer, *Message) error
 }
