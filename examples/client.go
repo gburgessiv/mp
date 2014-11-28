f7a708116c778541a8cfc04f697c0fb067ce1417
George Burgess IV <george.burgess.iv@gmail.com>
2014-11-27 23:19:24 -0500

    Added comments to examples

--- examples/client.go
+++ examples/client.go
@@ -1,104 +1,117 @@
 package main
 
 import (
-  "github.com/dyreshark/mp"
-  "os"
-  "log"
-  "fmt"
-  "io"
-  "net"
+	"fmt"
+	"github.com/dyreshark/mp"
+	"io"
+	"log"
+	"net"
+	"os"
 )
 
 // Client A is your typical echo client -- if it gets a message for protocol
 // "greetings", it'll wait for a message, display it, then send back "Hello World!"
 func startClientA(addr string) (io.Closer, error) {
-  conn, err := net.Dial("tcp", addr)
-  if err != nil {
-    log.Fatal("[A] ", err)
-  }
+	conn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal("[A] ", err)
+	}
 
-  mappedHandler := mp.NewMappedConnectionHandler()
-  mappedHandler.AddMapping("greetings", func(c mp.Connection) {
-    defer c.Close()
+	// We have the concept of MessageHandlers -- essentially, when one client
+	// wants to connect to another, it will send a "protocol" string with the
+	// connection request. MappedConnectionHandlers allow you to map a protocol
+	// to running an arbitrary function in a goroutine.
+	mappedHandler := mp.NewMappedConnectionHandler()
 
-    fmt.Println("[A] Got a new greetings connection. Someone wants to say hi!")
-    msg, err := c.ReadMessage()
-    if err != nil {
-      fmt.Println("[A] Instead of greeting us, they gave us this:", err)
-    }
+	// In this example, every time we get a connection request for the "greeting"
+	// protocol, we run the below func with the new connection
+	mappedHandler.AddMapping("greetings", func(c mp.Connection) {
+		defer c.Close()
 
-    fmt.Println("[A] We got", string(msg))
+		fmt.Println("[A] Got a new greetings connection. Someone wants to say hi!")
+		msg, err := c.ReadMessage()
+		if err != nil {
+			fmt.Println("[A] Instead of greeting us, they gave us this:", err)
+		}
 
-    err = c.WriteMessage([]byte("Hello World!"))
-    if err != nil {
-      fmt.Println("[A] We couldn't write back:", err)
-    } else {
-      fmt.Println("[A] We said hi back!")
-    }
-  })
+		fmt.Println("[A] We got", string(msg))
 
-  client := mp.NewClient("clientA", conn, mp.NewGobTranslator, mappedHandler)
-  client.Authenticate([]byte("No password needed!"))
+		err = c.WriteMessage([]byte("Hello World!"))
+		if err != nil {
+			fmt.Println("[A] We couldn't write back:", err)
+		} else {
+			fmt.Println("[A] We said hi back!")
+		}
+	})
 
-  go func() { log.Fatal(client.Run()) }()
-  return conn, nil
+	// Setting up the client -- its name will be clientA, it will talk to the
+	// server over the TCP connection we made (using Gob encoding -- see
+	// "encoding/gob" for details on that), and the new connection handler will
+	// be the mappedHandler we set up above.
+	client := mp.NewClient("clientA", conn, mp.NewGobTranslator, mappedHandler)
+	client.Authenticate([]byte("No password needed!"))
+	go func() { log.Fatal(client.Run()) }()
+	return conn, nil
 }
 
+// Client B spins up with the sole purpose of establishing a single Greetings
+// connection with clientA. It has a short conversation with clientA, and then
+// ends the connection. Ezpz.
 func runClientB(addr string) error {
-  netConn, err := net.Dial("tcp", addr)
-  if err != nil {
-    log.Fatal("[B]", err)
-    return err
-  }
+	netConn, err := net.Dial("tcp", addr)
+	if err != nil {
+		log.Fatal("[B]", err)
+		return err
+	}
 
-  mappedHandler := mp.NewMappedConnectionHandler()
-  // Don't want to accept any incoming connections
+	mappedHandler := mp.NewMappedConnectionHandler()
+	// Don't want to accept any incoming connections
 
-  client := mp.NewClient("clientB", netConn, mp.NewGobTranslator, mappedHandler)
-  defer client.Close()
+	client := mp.NewClient("clientB", netConn, mp.NewGobTranslator, mappedHandler)
+	defer client.Close()
 
-  client.Authenticate([]byte("No password needed!"))
-  go client.Run()
+	client.Authenticate([]byte("No password needed!"))
+	go client.Run()
 
-  conn, err := client.MakeConnection("clientA", "greetings")
-  if err != nil {
-    fmt.Println("[B] Couldn't connect to clientA -- ", err)
-    return err
-  }
+	conn, err := client.MakeConnection("clientA", "greetings")
+	if err != nil {
+		fmt.Println("[B] Couldn't connect to clientA -- ", err)
+		return err
+	}
 
-  err = conn.WriteMessage([]byte("Hello Friend!"))
-  if err != nil {
-    fmt.Println("[B] Error writing message", err)
-    return err
-  }
+	err = conn.WriteMessage([]byte("Hello Friend!"))
+	if err != nil {
+		fmt.Println("[B] Error writing message", err)
+		return err
+	}
 
-  msg, err := conn.ReadMessage()
-  if err != nil {
-    fmt.Println("[B] Error getting something back", err)
-    return err
-  }
+	msg, err := conn.ReadMessage()
+	if err != nil {
+		fmt.Println("[B] Error getting something back", err)
+		return err
+	}
 
-  fmt.Println("[B] They sent something back:", string(msg))
-  return nil
+	fmt.Println("[B] They sent something back:", string(msg))
+	return nil
 }
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Println("Usage: client port-number\n", os.Args[0])
-    return
-  }
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: client port-number\n", os.Args[0])
+		return
+	}
 
-  portStr := os.Args[1]
-  addr := "127.0.0.1:" + portStr
-  closer, err := startClientA(addr)
-  if err != nil {
-    log.Fatal("Couldn't start A:", err)
-  }
+	portStr := os.Args[1]
+	addr := "127.0.0.1:" + portStr
+	closer, err := startClientA(addr)
+	if err != nil {
+		log.Fatal("Couldn't start A:", err)
+	}
 
-  defer closer.Close()
+	defer closer.Close()
 
-  err = runClientB(addr)
-  if err != nil {
-    log.Fatal("Couldn't complete B:", err)
-  }
+	err = runClientB(addr)
+	if err != nil {
+		log.Fatal("Couldn't complete B:", err)
+	}
 }
--- examples/server.go
+++ examples/server.go
@@ -1,33 +1,35 @@
 package main
 
 import (
-  "github.com/dyreshark/mp"
-  "os"
-  "log"
-  "fmt"
-  "net"
+	"fmt"
+	"github.com/dyreshark/mp"
+	"log"
+	"net"
+	"os"
 )
 
 // An authentication function that will allow any client to connect.
 // Probably not the best idea in production...
 func authAny(name string, secret []byte) bool {
-  return true
+	return true
 }
 
 func main() {
-  if len(os.Args) < 2 {
-    fmt.Printf("Usage: server port-number", os.Args[0])
-    return
-  }
+	if len(os.Args) < 2 {
+		fmt.Printf("Usage: server port-number", os.Args[0])
+		return
+	}
 
-  // Validating that the 
-  portStr := os.Args[1]
-  addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:" + portStr)
-  if err != nil {
-    log.Fatal(err)
-  }
+	portStr := os.Args[1]
+	addr, err := net.ResolveTCPAddr("tcp", "0.0.0.0:"+portStr)
+	if err != nil {
+		log.Fatal(err)
+	}
 
-  listener, err := net.ListenTCP("tcp", addr)
-  server := mp.NewServer(authAny, mp.NewGobTranslator)
-  server.Listen(listener)
+	listener, err := net.ListenTCP("tcp", addr)
+	// Create a new server that will allow any client name/secret combo to connect
+	// to it (probably not the best idea), and that speaks using gob encoding
+	// (see docs for "encoding/gob" for more info)
+	server := mp.NewServer(authAny, mp.NewGobTranslator)
+	log.Fatal(server.Listen(listener))
 }
